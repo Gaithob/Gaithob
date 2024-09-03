2b934c5a6c64dc7aab63bb11f35f853d31f9cebe
Gaithob <gaithob@gmail.com>
2024-09-03 00:19:45 -0400

    Update database.sql
    Insert statement

--- database.sql
+++ database.sql
@@ -36,8 +36,29 @@ DROP TABLE IF EXISTS companies;
 CREATE TABLE IF NOT EXISTS companies(
 	company_id VARCHAR(40) NOT NULL,
     company_name VARCHAR(40) NOT NULL,
-    headquarters_phone_number INT,
+    headquarters_phone_number VARCHAR(50) DEFAULT NULL,
     PRIMARY KEY (company_id)
 );
 
-    
+INSERT INTO sales VALUES (1, '2015-01-01', 1, 01);
+INSERT INTO sales VALUES (2, '2015-01-02', 2, 02);
+INSERT INTO sales VALUES (3, '2015-02-02', 3, 03);
+INSERT INTO sales VALUES (4, '2015-02-03', 2, 02);
+INSERT INTO sales VALUES (5, '2015-03-02', 4, 04);
+
+INSERT INTO customers VALUES (1, 'John', 'Taveta', 'M', 'john@email.com', 2);
+INSERT INTO customers VALUES (2, 'Davie', 'Masinga', 'M', 'davie@email.com', 3);
+INSERT INTO customers VALUES (3, 'Waja', 'Wawaya', 'F', 'waja@email.com',1);
+INSERT INTO customers VALUES (4, 'Vera', 'Akothe', 'F', 'akothe@email.com', 6);
+INSERT INTO customers VALUES (5, 'Andrew', 'Atwoli', 'M', 'atwoli@email.com', 4);
+INSERT INTO customers VALUES (6, 'Lilian', 'Nyamu', 'F', 'lilian@email.com',5);
+
+INSERT INTO items VALUES (01, 3, 6.15, '1');
+INSERT INTO items VALUES (02, 5, 10.25, 2);
+INSERT INTO items VALUES (03, 9, 18.45, 3);
+INSERT INTO items VALUES (04, 2, 4.10, 2);
+
+INSERT INTO companies VALUES (1, 'City Treats', '192-168-8080');
+INSERT INTO companies VALUES (2, 'Amazoo Sweets', '614-001-0000');
+INSERT INTO companies VALUES (3, 'Tropical Candy', '300-000-1111');
+
